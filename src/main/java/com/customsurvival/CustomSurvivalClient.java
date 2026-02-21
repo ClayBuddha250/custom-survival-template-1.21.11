0302d49df91645f420c9079a1e804f7f9757ffca
日文 邱 <minecraftgamerule@qq.com>
2026-02-21 18:12:05 +0800

    Initial commit

---
+++ src/main/java/com/customsurvival/CustomSurvivalClient.java
@@ -0,0 +1,10 @@
+package com.customsurvival;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomSurvivalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
